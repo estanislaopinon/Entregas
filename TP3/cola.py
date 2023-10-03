929b26a16ec5dd58c6f625bf86c35d60a5107c99
estanislaopinon <estanislaopinon@gmail.com>
2023-10-03 13:25:34 -0300

    cambio TP3

--- TP3/cola.py
+++ TP3/cola.py
@@ -6,24 +6,21 @@ class Cola():
     def __init__(self):
         self.elementos = []
 
-    #agrega el elemento al final de la cola
-    def arrive(self, value):
+    def arrive(self, value):  # Agrega el elemento al final de la cola
         self.elementos.append(value)
 
-    #elimina y devuelve el elemento almacenado en el frente de la cola
-    def atention(self):
+    def atention(self):  # Elimina y devuelve el elemento almacenado en el frente de la cola
         if self.size() > 0:
             return self.elementos.pop(0)  # elimina el primero
-    
-    def size(self):
+
+    def size(self):  # devuelve la cantidad de elementos en la cola
         return len(self.elementos)
 
-    #devuelve el valor del elemento que esta almacenado en el frente de la cola sin eliminarlo
-    def on_front(self):
+    def on_front(self):  # Devuelve el valor del elemento que esta almacenado en el frente de la cola sin eliminarlo
         if self.size() > 0:
             return self.elementos[0]
 
-    #elimina el elemento en el frente de la cola y lo inserta en el final de la misma
+    # elimina el elemento en el frente de la cola y lo inserta en el final de la misma
     def move_to_end(self):
         if self.size() > 0:
             aux = self.atention()
@@ -37,3 +34,27 @@ cola = Cola()
 for i in range(5):
     value = randint(1, 50)
     cola.arrive(value)
+
+# print()
+# print(cola.atention())
+# print(cola.atention())
+# print(cola.on_front())
+# print(cola.elementos)
+# print(cola.size())
+
+# while cola.size()>0:
+#     value = cola.atention()
+#     print(value)
+#     cola_aux.arrive(value)
+
+# print(cola.size())
+# print(cola_aux.elementos)
+# print(cola.elementos)
+# print()
+# cont=0
+# while cont< cola.size():
+#     value = cola.move_to_end()
+#     print(value)
+#     cont+=1
+
+# print(cola.elementos)
--- TP3/ej1_cola.py
+++
@@ -1,4 +0,0 @@
-# Eliminar de una cola de caracteres todas las vocales que aparecen.
-from cola import Cola
-from random import randint
-letras = Cola()
